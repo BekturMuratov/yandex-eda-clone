58590d419d354c29b693baefe22e54192e0aebc4
BekturMuratov <Muratov225@gmail.com>
2020-12-13 20:21:52 +0600

    header,banner,food-navigation is ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AddressSearchComponent } from './components/address-search/address-search.component';
+import { FoodNavigationComponent } from './components/food-navigation/food-navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AddressSearchComponent,
+    FoodNavigationComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/food-navigation/food-navigation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-food-navigation',
+  templateUrl: './food-navigation.component.html',
+  styleUrls: ['./food-navigation.component.css']
+})
+export class FoodNavigationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
